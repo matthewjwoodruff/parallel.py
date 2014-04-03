edff52b5d148b216fc92facf9d197bb382fdc390
Matthew Woodruff <mjw5407@psu.edu>
2014-04-02 21:45:59 -0400

    License header specifies parallel.py

--- parallel.py
+++ parallel.py
@@ -1,18 +1,18 @@
 """
 Copyright (C) 2013 Matthew Woodruff
 
-This script is free software: you can redistribute it and/or modify
+parallel.py is free software: you can redistribute it and/or modify
 it under the terms of the GNU Lesser General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
 
-This script is distributed in the hope that it will be useful,
+parallel.py is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 GNU Lesser General Public License for more details.
 
 You should have received a copy of the GNU Lesser General Public License
-along with this script. If not, see <http://www.gnu.org/licenses/>.
+along with parallel.py. If not, see <http://www.gnu.org/licenses/>.
 
 """
 import math
